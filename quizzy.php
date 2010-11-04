80265d6555c5af268a217fe30908393224ee6f50
Joe Balough <scallopedllama@gmail.com>
2010-11-04 21:10:26 +0900

    added license headers to all the files.. something that should have been there long ago.

--- quizzy.php
+++ quizzy.php
@@ -1,5 +1,23 @@
 <?php
 
+  /*
+   * This file is part of quizzy.
+   *
+   * quizzy is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU Affero General Public License as 
+   * published by the Free Software Foundation, either version 3 of
+   * the License, or (at your option) any later version.
+   *
+   * quizzy is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU Affero General Public License for more details.
+   *
+   * You should have received a copy of the GNU Affero General Public 
+   * License along with quizzy. If not, see <http://www.gnu.org/licenses/>.
+   */
+
+
   // Global variables:
   // This string represents where this quiz's pictures should be found
   $pic_dir = $quizzy_cwd . '/' . $quiz_folder . '/' . $pic_folder . '/';
