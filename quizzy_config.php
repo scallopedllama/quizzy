d377e202c19e57730563a4076aeebb6a9338db7a
Joe Balough <scallopedllama@gmail.com>
2010-11-04 14:33:51 +0900

    renamed variables to conform to new style. All php variables are of the word_word all lower-case style and all js variables are camelCaseStyle. Also, all global varibles are prefixed with quizzy_ and quizzy respectively to avoid collisions with other people's code.

--- quizzy_config.php
+++ quizzy_config.php
@@ -7,28 +7,28 @@
   
   // where the quiz xml files are stored under the /quizzy directory
   // default is /quizzy/quizzes so by default the variable is set to 'quizzes'
-  $quizFolder = 'quizzes';
+  $quizzy_quiz_folder = 'quizzes';
   
   // where the quiz picture files are stored under /quizzy/$quizFolder
   // by default, it looks in the same folder as the quizzes. this might get
   // messy with a large number of quizzes so the option to move it out is given here
-  $picFolder = '.';
+  $quizzy_pic_folder = '.';
   
   // the dimensions of the quiz in pixels
-  $quizWidth = 200;
-  $quizHeight = 300;
+  $quizzy_quiz_width = 200;
+  $quizzy_quiz_height = 300;
   
   // The message to display above the list of quiz names that the user would select
   // this is put in an h1 tag
-  $pickQuizMessage = 'Please Select a Quiz';
+  $quizzy_pick_quiz_message = 'Please Select a Quiz';
   
   // The message displayed at the end of the quiz before the user's score, grade, and rank
   // this is put in an h1 tag
-  $endQuizMessage = 'Done!';
+  $quizzy_end_quiz_message = 'Done!';
 
   // Keeps track of the current working directory.
   // This bit here will prevent breakage on windows by replacing '\' characters with '/' characters
-  $cwd = str_replace('\\', '/', getcwd());
+  $quizzy_cwd = str_replace('\\', '/', getcwd());
   
   /**
    *  JavaScript-Only Global Variables
@@ -36,28 +36,28 @@
    *  This PHP array contains all of the variables that are needed by the JavaScript side of
    *  quizzy. It is kept here for ease of configuration and is passed to jQuery via JSON encoding.
    */
-  $js_variables = array(
+  $quizzy_js_variables = array(
   
     // how fast fading animations should be completed (in ms)
-    'fadeSpeed' => 'def',
+    'quizzyFadeSpeed' => 'def',
 
     // how fast sliding animations should be completed (in ms)
-    'slideSpeed' => 'def',
+    'quizzySlideSpeed' => 'def',
 
     // how to animate movement. can be linear or swing
-    'animateStyle' => 'swing',
+    'quizzyAnimateStyle' => 'swing',
 
     // how long to wait in ms before scrolling up non-100% options
-    'slideUpWait' => 500,
+    'quizzySlideUpWait' => 500,
 
     // how long to wait in ms before fading in the explanation
-    'expFadeInWait' => 200,
+    'quizzyExpFadeInWait' => 200,
 
     // how long to wait in ms before fading in the next button
-    'nextFadeInWait' => 500,
+    'quizzyNextFadeInWait' => 500,
 
     // time in ms it takes for the quiz to restart
-    'restartSpeed' => 500,
+    'quizzyRestartSpeed' => 500,
 
 
     // SETTINGS for jQuery Loading
@@ -72,16 +72,21 @@
     // 'fade error'			-- displays 'Loading...' and fades the div in and out for 100 seconds and changes the message to a static 'Task may have failed.'
     // 'working type' 		-- "types" 'Loading...' for 10 seconds then changes to "type" 'Still Working...'
     //  note that these can generally be combined to produce the desired effect like how 'working type' is a combination of 'working' and 'type'
-    'loadingPulse' => 'ellipsis',
+    'quizzy_LoadingPulse' => 'ellipsis',
 
     // where to put the loading message. in format of '[vertical align]-[horizontal align]' unless center center then it's just 'center'
     // vertical line can be 'top', 'center', or 'bottom', horizontal align can be 'left', 'center', or 'right'
     // so if you want it in the top left, you set loadingAlign to 'top-left'.
-    'loadingAlign' => 'bottom-left',
+    'quizzyLoadingAlign' => 'bottom-left',
 
     // how long to wait before putting the loading message up in milliseconds.
     // This setting will not do anything as of this version because the delay feature of loading is broken.
-    'loadingDelay' => 300,
+    'quizzyLoadingDelay' => 300,
+    
+    // The width and height of the quiz window. These are set above so their PHP variable values are simply used here.
+    // You shouldn't change this.
+    'quizzyQuizWidth' => $quiz_width,
+    'quizzyQuizHeight' => $quiz_height,
     
   ); // js_variables
   
