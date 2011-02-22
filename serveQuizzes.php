574d7db27d59e084541a818411aad8e429c50312
Joe Balough <scallopedllama@gmail.com>
2011-02-22 14:23:27 +0900

    a bunch of whitespace cleanups done automatically by kate and a backport of the fix for #16

--- serveQuizzes.php
+++ serveQuizzes.php
@@ -1,29 +1,29 @@
-<?php 
+<?php
   include 'quizzyConfig.php';
   //open the quizzes dir
   $quizDir = dir($quizFolder);
-  
+
   //represents where this quiz's pictures should be found
   $picDir = 'quizzy/'.$quizFolder.'/'.$picFolder.'/';
-  
+
   echo '<div class="quizzy_load_body">';
 	echo '<h1>'.$pickQuizMessage.'</h1>';
-  
+
   //List files in quizzes directory
   $j = 0;
   while (($file = $quizDir->read()) !== false)
   {
   	//don't try to open . or ..
   	if(( $file == '.')||( $file == '..') ) continue;
-  	
+
     //make sure we have proper extension
-    if(!strpos(strtolower($file), 'xml'))
+    if (!preg_match("/.xml$/i", $file))
       continue;
-    
+
   	//open the current file
   	$filename = $cwd.'/'.$quizFolder.'/'.$file;
   	$quizXML= simplexml_load_file($filename);
-  	
+
   	//make a list of all the quizzes in this xml file
   	$i=0;
   	foreach ($quizXML->quiz as $curQuiz){
@@ -32,17 +32,17 @@
   		  <input type="radio" class="quizzy_quiz_opt" id="quizzy_quiz_opt<?php echo $j; ?>" onClick="quizFile = '<?php echo basename($filename); ?>'; quizIndex = <?php echo $i; ?>;" name="quizzy_quiz_sel">
         <label class="quizzy_quiz_lbl" id="quizzy_quiz_lbl<?php echo $j; ?>"><?php echo $curQuiz->title; ?></label>
         <?php if(isset($curQuiz->img)) { ?>
-          <img src="<?php echo $picDir . $curQuiz->img['src']; ?>" alt="<?php echo $curQuiz->img['alt']; ?>" > 
-        <?php 
+          <img src="<?php echo $picDir . $curQuiz->img['src']; ?>" alt="<?php echo $curQuiz->img['alt']; ?>" >
+        <?php
         }
-        if(isset($curQuiz->description)) { 
+        if(isset($curQuiz->description)) {
         ?>
         <br >
           <div id="quizzy_quiz_desc<?php echo $j; ?>" class="quizzy_quiz_desc">
-            <?php 
-              if(isset($curQuiz->description->img)) { 
+            <?php
+              if(isset($curQuiz->description->img)) {
                 echo '<img src="'.$picDir.$curQuiz->description->img['src'].'" alt="'.$curQuiz->description->img['alt'].'" >';
-              } 
+              }
             ?>
             <?php echo $curQuiz->description->text; ?>
           </div>
@@ -51,7 +51,7 @@
 <?php
   		++$i; ++$j;
   	}
-  
+
   }
 ?></div>
     <div class="quizzy_load_foot"><input type="submit" class="quizzy_b" id="quizzy_start_b" value="Start Quiz"></div>
